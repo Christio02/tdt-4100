42089018e8e57a9b9802c91129e6ea5df5efc17f
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-01-17 12:30:27 +0100

    Finished oving1

--- ovinger/src/main/java/oving1/UpOrDownCounter.java
+++ ovinger/src/main/java/oving1/UpOrDownCounter.java
@@ -2,13 +2,17 @@ package oving1;
 
 public class UpOrDownCounter {
 
-    int start; // tilstander 
+    int start; // tilstander
     int end;
     int counter;
 
-    public UpOrDownCounter(int start, int end) {
+    public UpOrDownCounter(int start, int end) { // constructor
         this.counter = start;
         this.end = end;
+
+        if (counter == end) {
+            throw new IllegalArgumentException("Kan ikke ha lik start og end verdi");
+        }
     }
 
     public int getCounter() {
@@ -16,26 +20,41 @@ public class UpOrDownCounter {
     }
 
     public boolean count() {
-        if (this.counter > this.end) {
-            this.counter--;
-            return true;    
+        if (this.counter + 1 == this.end) { // sjekker om counter == end, må ta med + 1 for å fortsatt counte oppover
+            counter++;
+            return false;
 
-        } else if (this.counter < this.end) {
+        }
+        if (this.counter - 1 == this.end) {
+            counter--;
+            return false;
+
+        } else if (this.counter < this.end) { // hvis den er mindre eller større, return true;
             this.counter++;
             return true;
 
+        } else if (this.counter > this.end) {
+            this.counter--;
+            return true;
         } else {
             return false;
         }
+
+    }
+
+    @Override // gjør om til string
+    public String toString() {
+        return "Counter: " + " " + counter + " " + "End: " + " " + end;
     }
 
     public static void main(String[] args) {
-        UpOrDownCounter UpOrDown = new UpOrDownCounter(3, 1);
-        System.out.println(UpOrDown.getCounter());
+        UpOrDownCounter UpOrDown = new UpOrDownCounter(-3, -1);
+        System.out.println(UpOrDown);
+        UpOrDown.count();
+        System.out.println(UpOrDown);
+        UpOrDown.count();
+        System.out.println(UpOrDown);
         System.out.println(UpOrDown.count());
-        System.out.println(UpOrDown.getCounter());
-        System.out.println(UpOrDown.count());
-        System.out.println(UpOrDown.getCounter());
-        System.out.println(UpOrDown.count());
+
     }
 }
