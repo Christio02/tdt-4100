c8d35c0ab08771ea4880bdc672dc6b3f3dc9a80b
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-01-24 13:34:02 +0100

    Changed states for Person.java, and implemented number check for name

--- ovinger/src/main/java/oving2/Person.java
+++ ovinger/src/main/java/oving2/Person.java
@@ -3,42 +3,65 @@ package oving2;
 import java.util.Date;
 public class Person {
     // states
-    private String fullname;
-    private String fornavn;
-    private String etternavn;
-    private String epost;
+    private String name;
+    private String email;
 
-    private Date fodselsDag;
-    private char kjonn;
+    private Date birthdate;
+    private char gender;
 
     // constructior
 
-    public Person(String fornavn, String etternavn, String epost, Date fodselsDag, char kjonn) {
-        this.fornavn = fornavn;
-        this.etternavn = etternavn;
-        this.epost = epost;
-        this.fodselsDag = fodselsDag;
-        this.kjonn = kjonn;
+    public Person(String firstname, String lastname) {
+        setName(firstname, lastname); // remember to call method in constructor
     }
 
     // setters
 
-    // sets fullname
-    public void setName(String fornavn, String etternavn) {
-        if (fornavn.length() < 2 || etternavn.length() < 2) {
+    // sets name
+    public void setName(String firstname, String lastname) {
+        if (firstname.length() < 2 || lastname.length() < 2) {
             throw new IllegalArgumentException("Fornavn og etternavn må være på minst 2 bokstaver!");
         }
-        this.fornavn = fornavn;
-        this.etternavn = etternavn;
-        this.fullname = this.fornavn + " " + this.etternavn;
+        this.name = firstname + " " + lastname;
+
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+
+            if (Character.isDigit(c)) { // goes through fullname and checks if it contains numbers
+                throw new IllegalArgumentException("Fornavn eller etternavn inneholder tall!");
+            }
+        }
     }
 
-    
+    // gets fullname
+    public String getName() {
+        return name;
+    }
 
 
 
 
 
-    
+    public void setEmail(String email) {
+
+
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Chris", "Høe");
+        System.out.println(p1);
+        //should throw error
+        // Person p2 = new Person("D", "H");
+
+    }
 
 }
