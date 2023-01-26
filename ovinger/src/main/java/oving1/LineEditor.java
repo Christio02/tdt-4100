fde61d541f185553af496999bea816b0c2237bb5
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-01-26 17:33:07 +0100

    Continued on lineEditor, need more fixing

--- ovinger/src/main/java/oving1/LineEditor.java
+++ ovinger/src/main/java/oving1/LineEditor.java
@@ -24,15 +24,18 @@ public class LineEditor {
     }
 
     public void setInsertionIndex(int insertionIndex) {
+
         this.insertionIndex = insertionIndex;
     }
 
     public void left() {
-        this.insertionIndex -= 1;
+        this.insertionIndex = Math.max(0, insertionIndex - 1); // sets insertionindex altways to 0, can't be lower
+
     }
 
     public void right() {
-        this.insertionIndex += 1;
+        this.insertionIndex = Math.min(text.length(), insertionIndex + 1); // sets insertionindex always to 0, cant't be
+                                                                           // higher than text length
     }
 
     public void insertString(String s) {
@@ -40,18 +43,49 @@ public class LineEditor {
                                                                     // would be
         String end = getText().substring(insertionIndex); // sets the start to insertionIndex
         this.text = begin + s + end;
-        // need to set textposition to new string, need to move right of new text
-        // setInsertionIndex(s.length() + 1);
+
+        insertionIndex += s.length(); // sets insertionIndex to substring length
+    }
+
+    public void deleteLeft() { // need to fix this
+        StringBuilder s = new StringBuilder(text);
+        if (insertionIndex == 0 || text.length() > 1) {
+            throw new IllegalArgumentException("Kan ikke slette tegn til venstre på index 0!");
+        }
+
+        if (insertionIndex > 0) {
+            s.deleteCharAt(insertionIndex - 1);
+            insertionIndex--;
+
+        } else {
+            s.deleteCharAt(--insertionIndex);
+            String newS3 = s.toString();
+            text = newS3;
+            insertionIndex = insertionIndex - 1;
+        }
 
     }
 
-    public void deleteLeft() {
-        for (int i = 0; i < text.length(); i++) {
-            if (i == insertionIndex) {
-                String newS = text.substring(0, i) + text.substring(i + 1);
-                this.text = newS;
-            }
+    public void deleteRight() {
+        if (text.length() == 0) {
+            return;
         }
+        StringBuilder s = new StringBuilder(text);
+        if (text.length() == 1 || insertionIndex == 0) {
+            s.deleteCharAt(insertionIndex);
+            String newS = s.toString();
+            text = newS;
+        } else {
+            s.deleteCharAt(insertionIndex + 1);
+            String newS2 = s.toString();
+            text = newS2;
+            insertionIndex = insertionIndex + 1;
+        }
+
+    }
+
+    public int returnLength() {
+        return this.text.length();
     }
 
     @Override
@@ -64,12 +98,10 @@ public class LineEditor {
 
     public static void main(String[] args) {
         LineEditor l = new LineEditor();
-        l.setText("Javagøy!");
-        l.setInsertionIndex(4);
-        l.insertString("er");
-        System.out.println(l.getInsertionIndex());
-        System.out.println(l);
-
+        l.setText("");
+        l.insertString("J");
+        l.setInsertionIndex(0);
+        l.deleteLeft();
     }
 
 }
