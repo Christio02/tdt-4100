f70f3455db745c68a656ba6a741398b041b42454
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-02-08 16:48:14 +0100

    don't know what this is

--- ovinger/src/test/java/oving4/PartnerTest.java
+++ ovinger/src/test/java/oving4/PartnerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import oving4.partner.Partner;
+
 public class PartnerTest {
 
 	private Partner p1;
