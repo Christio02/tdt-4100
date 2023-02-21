de3cc385b7ea25b55f412f6ab4e88a312c611ce0
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-02-21 15:21:13 +0100

    Started on delegation - office assignment, need to fix printer.java

---
+++ ovinger/src/main/java/oving6/delegation/office/Clerk.java
@@ -0,0 +1,39 @@
+package oving6.delegation.office;
+
+import java.util.function.BinaryOperator;
+
+public class Clerk implements Employee{
+
+    private Printer printer;
+    private int taskCount = 0;
+
+
+    public Clerk(Printer printer) {
+        this.printer = printer;
+    }
+
+
+
+    @Override
+    public double doCalculations(BinaryOperator<Double> operation, double value1, double value2) {
+        taskCount++;
+        return operation.apply(value1, value2);
+    }
+
+    @Override
+    public void printDocument(String document) {
+
+        printer.printDocument(document, this); // prints document for this clerk
+    }
+
+    @Override
+    public int getTaskCount() {
+       return this.taskCount;
+    }
+
+    @Override
+    public int getResourceCount() {
+        return 1;
+    }
+    
+}
---
+++ ovinger/src/main/java/oving6/delegation/office/Printer.java
@@ -0,0 +1,27 @@
+package oving6.delegation.office;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Printer {
+
+    private Employee employee;
+    private List<String> listOfDocuments= new ArrayList<String>();
+
+    public void printDocument(String document, Employee employee) {
+        System.out.println("Document: " + " " + document);
+        if (!listOfDocuments.contains(document)) {
+            listOfDocuments.add(document);
+        }
+        
+    }
+
+    public List<String> getPrintHistory(Employee employee) {
+        if (employee.getTaskCount() == 0) {
+            return List.of(); // returns empty list of employee has no tasks
+        }
+        // should return list of all documents printed by employee;
+        return this.listOfDocuments;
+    }
+
+}
