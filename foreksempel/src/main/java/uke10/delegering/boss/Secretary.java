1d3f0ad3a56aad559e451d6b24b67c11066a0e8b
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-03-06 10:21:58 +0100

    Added lecture code, monday week 10

--- foreksempel/src/main/java/uke10/delegering/boss/Secretary.java
+++ foreksempel/src/main/java/uke10/delegering/boss/Secretary.java
@@ -4,7 +4,7 @@ public class Secretary extends Worker {
 
 	@Override
 	public String work() {
-		return "Dette er arbeidet til sekretæren.";
+		return super.work();
 		// Hva vil super være en referanse til, og hva skjer om vi kaller super.work()?
 	}
 
